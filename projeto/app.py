41b07860119263d87bf074039ba4400a163dba5e
Rafael Arantes <rafaelarantes2016@gmail.com>
2025-12-03 21:58:00 -0300

    Projeto dia 2

--- projeto/app.py
+++ projeto/app.py
@@ -1,6 +1,6 @@
 import json, os
 from flask import Flask, render_template, request, redirect, url_for
-from ferramentas.ferramentas import calcular #Se colocar "*" ela importa tudo que estiver dentro
+from ferramentas.ferramentas import * #Se colocar "*" ela importa tudo que estiver dentro
 
 app = Flask(__name__)
 
@@ -23,6 +23,31 @@ def calculadora():
 @app.route('/ferramentas', methods=['GET', 'POST'])
 def ferramentas():
     context = {}
+
+    if request.method == "POST":
+        if "verificar_par_impar" in request.form:
+            num = int(request.form["num"])
+            context["par_impar_result"] = parImpar(num)
+        elif "calc_imc" in request.form:
+            peso = float(request.form["peso"])
+            altura = float(request.form["altura"])
+            context["imc_result"] = calcImc(peso,altura)
+        elif "gerar_tabuada" in request.form:
+            num = int(request.form["num"])
+            context["tabuada_result"] = calcTabuada(num)
+        elif "calc_potencia" in request.form:
+            base = float(request.form["base"])
+            expoente = int(request.form["expoente"])
+            context["potencia_result"] = calcPotencia(base,expoente)
+        elif "calc_combustivel" in request.form:
+            etanol = float(request.form["etanol"])
+            gasolina = float(request.form["gasolina"])
+            context["combustivel_result"] = calcCombustivel(etanol,gasolina)
+        #elif "convert_temp" in request.form:
+         #   temp = float(request.form["temp"])
+          #  escala = request.form["escala"]
+           # context["temp_result"] = calcTemp(temp,escala)
+
     return render_template('ferramentas.html', context=context)
     
 
--- projeto/ferramentas/ferramentas.py
+++ projeto/ferramentas/ferramentas.py
@@ -21,3 +21,52 @@ def calcular(num1,num2,operacao):
         return f"{num1} % {num2} = {num1%num2}"
     else:
         return "Operação Inválida!"
+
+def parImpar(num):
+    if num%2==0:
+        return f"O número {num} é Par!"
+    else:
+        return f"O número {num} é Impar!"
+
+def calcImc(peso,altura):
+    imc = round(peso / (altura * altura),2)
+    if imc < 16:
+        return f"O seu IMC é de {imc}, e está classificado como Magreza Grave!"
+    elif imc >= 16 and imc < 17:
+        return f"O seu IMC é de {imc}, e está classificado como Magreza Moderada!"        
+    elif imc >= 17 and imc < 18.5:
+        return f"O seu IMC é de {imc}, e está classificado como Magreza Leve!"        
+    elif imc >= 18.5 and imc < 25:
+        return f"O seu IMC é de {imc}, e está classificado como Saudável!"       
+    elif imc >= 25 and imc < 30:
+        return f"O seu IMC é de {imc}, e está classificado como Sobrepeso!"        
+    elif imc >= 30 and imc < 35:
+        return f"O seu IMC é de {imc}, e está classificado como Obesidade Grau I!"        
+    elif imc >= 35 and imc < 40:
+        return f"O seu IMC é de {imc}, e está classificado como Obesidade Grau II!"    
+    elif imc >= 40:
+        return f"O seu IMC é de {imc}, e está classificado como Obesidade Grau III!"        
+    else:
+        return f"Valor inválido!"
+
+def calcTabuada(num):
+    resultado = []
+    for i in range(1,11):
+        resultado.append(f"{num} x {i} = {num*i}")
+    return resultado
+
+def calcPotencia(base,expoente):
+    return f"O {base} elevado a {expoente} é {base ** expoente}"
+
+def calcCombustivel(etanol,gasolina):
+    combustivel = etanol / gasolina
+    if combustivel <= 0.7:
+        return f"Com base nos preços informados, será mais econômico abastecer com Etanol!"
+    else:
+        return f"Com base nos preços informados, será mais econômico abastecer com Gasolina!"
+
+#def calcTemp(temp,escala):
+ #   if escala == celsius:
+  #      return f"A temperatura {temp}ºF convertido Celsius é {(temp - 32)/1.8}ºC"
+   # else:
+    #    return f"A temperatura {temp}ºC convertido é Fahrenheit {temp * 1.8 + 32}ºF"
